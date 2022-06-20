febe893c41c7e08d82fc5b347409287c6d17ce7f
Yoshihisa Kimura <kimura4413@gmail.com>
2022-06-20 10:18:22 +0900

    Update PerfectErase.py
    Renamed the function to overwrite with a specific number.

--- PerfectErase.py
+++ PerfectErase.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import random
-def overWrite(filePath, data):
+def overWriteSpecific(filePath, data):
     contentLength = os.path.getsize(filePath)
     with open(filePath, "r+b") as f:
         for i in range(contentLength):
@@ -18,6 +18,6 @@ def overWriteRandom(filePath):
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 2:
-        overWrite(args[1], b'\x00')
-        overWrite(args[1], b'\xff')
+        overWriteSpecific(args[1], b'\x00')
+        overWriteSpecific(args[1], b'\xff')
         overWriteRandom(args[1])
