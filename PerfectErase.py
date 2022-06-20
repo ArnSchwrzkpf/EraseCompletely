5291a29af85d2e15fa919c32cb852e1c630a29cd
Yoshihisa Kimura <kimura4413@gmail.com>
2022-06-20 10:03:33 +0900

    Create PerfectErase.py
    Implemented the minimum required functions.

---
+++ PerfectErase.py
@@ -0,0 +1,23 @@
+# PerfectErase PY
+import os
+import sys
+import random
+def overWrite(filePath, data):
+    contentLength = os.path.getsize(filePath)
+    with open(filePath, "r+b") as f:
+        for i in range(contentLength):
+            f.write(data)
+        f.flush()
+def overWriteRandom(filePath):
+    contentLength = os.path.getsize(filePath)
+    with open(filePath, "r+b") as f:
+        for i in range(contentLength):
+            data = random.randrange(256).to_bytes(1, 'little')
+            f.write(data)
+        f.flush()
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 2:
+        overWrite(args[1], b'\x00')
+        overWrite(args[1], b'\xff')
+        overWriteRandom(args[1])
